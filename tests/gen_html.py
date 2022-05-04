5672292f3d12f28e32725e330197584e4ea0cfa8
Nick DeRobertis <whoopnip@gmail.com>
2022-05-04 18:12:28 -0400

    Add an fxt init-from HTML that is currently not working

--- tests/gen_html.py
+++ tests/gen_html.py
@@ -1,5 +1,5 @@
 from terminhtml.main import TerminHTML
-from tests.config import RICH_HTML
+from tests.config import RICH_HTML, FXT_INIT_FROM_HTML
 
 
 def create_rich_html() -> str:
@@ -8,5 +8,12 @@ def create_rich_html() -> str:
     return str(term)
 
 
+def create_fxt_init_from_html() -> str:
+    commands = ["fxt init-from -n https://github.com/nickderobertis/copier-typescript-npm-sphinx"]
+    term = TerminHTML.from_commands(commands)
+    return str(term)
+
+
 if __name__ == "__main__":
     RICH_HTML.write_text(create_rich_html())
+    FXT_INIT_FROM_HTML.write_text(create_fxt_init_from_html())
