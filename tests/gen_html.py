66955b37b50fd6aafda189f68bf31e6c621c3595
Nick DeRobertis <whoopnip@gmail.com>
2022-05-06 06:54:37 -0400

    Add fxt update example, not causing a problem

--- tests/gen_html.py
+++ tests/gen_html.py
@@ -1,5 +1,11 @@
 from terminhtml.main import TerminHTML
-from tests.config import RICH_HTML, FXT_INIT_FROM_HTML, FXT_ADD_OUTPUT_HTML, BASIC_HTML
+from tests.config import (
+    RICH_HTML,
+    FXT_INIT_FROM_HTML,
+    FXT_ADD_OUTPUT_HTML,
+    BASIC_HTML,
+    FXT_UPDATE_HTML,
+)
 
 
 def create_basic_html() -> str:
@@ -10,6 +16,7 @@ def create_basic_html() -> str:
     ]
     term = TerminHTML.from_commands(commands)
     return str(term)
+
 
 def create_rich_html() -> str:
     commands = ["python -m rich"]
@@ -48,8 +55,33 @@ def create_fxt_add_output_html() -> str:
     return str(term)
 
 
+def create_fxt_update_html() -> str:
+    setup_commands = [
+        "git init",
+        "touch woo.txt",
+        "git add .",
+        "git commit -m 'Initial commit'",
+        "fxt init-from https://github.com/nickderobertis/copier-simple-example --no-input --version c7e1ba1bfb141e9c577e7c21ee4a5d3ae5dde04d --folder-name my-project && cd my-project && fxt config target copier-simple-example",
+    ]
+    commands = [
+        "cat answer1.txt",
+        "fxt update",
+        "cat answer1.txt",
+    ]
+    input = [
+        None,
+        "\n50",
+    ]
+    prompt_matchers = ["]: ", r"0m: "]
+    term = TerminHTML.from_commands(
+        commands, setup_commands, input, prompt_matchers=prompt_matchers
+    )
+    return str(term)
+
+
 if __name__ == "__main__":
-    BASIC_HTML.write_text(create_basic_html())
-    RICH_HTML.write_text(create_rich_html())
-    FXT_INIT_FROM_HTML.write_text(create_fxt_init_from_html())
-    FXT_ADD_OUTPUT_HTML.write_text(create_fxt_add_output_html())
+    # BASIC_HTML.write_text(create_basic_html())
+    # RICH_HTML.write_text(create_rich_html())
+    # FXT_INIT_FROM_HTML.write_text(create_fxt_init_from_html())
+    # FXT_ADD_OUTPUT_HTML.write_text(create_fxt_add_output_html())
+    FXT_UPDATE_HTML.write_text(create_fxt_update_html())
