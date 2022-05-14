2441f23dce5f1242eb893e676602554bf4189025
Nick DeRobertis <whoopnip@gmail.com>
2022-05-14 00:00:56 -0400

    Dynamically generate demo output gif for README

---
+++ demo_video.py
@@ -0,0 +1,59 @@
+import tempfile
+import time
+from pathlib import Path
+
+from playwright.sync_api import sync_playwright
+import moviepy.editor as mp
+from terminhtml.main import TerminHTML
+
+PROJECT_ROOT = Path(__file__).parent
+DOCS_IMAGES = PROJECT_ROOT / "docsrc" / "source" / "_static" / "images"
+VIDEOS_DIR = PROJECT_ROOT / "videos"
+
+
+def create_demo_output_gif(out_folder: Path = DOCS_IMAGES):
+    print("Creating demo output gif")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        temp_path = Path(tmpdir)
+        term = TerminHTML.from_commands(
+            ["python -m terminhtml.demo_output"],
+            cwd=PROJECT_ROOT,
+            prompt_matchers=["\\[0m: "],
+            input=["Nick DeRobertis"],
+        )
+        html_path = temp_path / "termin.html"
+        html_path.write_text(term.to_html())
+
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            dimensions = dict(width=800, height=530)
+            context = browser.new_context(
+                viewport=dimensions,
+                record_video_dir=str(temp_path.resolve()),
+                record_video_size=dimensions,
+            )
+            page = context.new_page()
+            page.goto(html_path.as_uri())
+            speed_up = page.locator("text=►")
+            speed_down = page.locator("text=◄")
+            time.sleep(1)
+            speed_up.click()
+            time.sleep(1)
+            speed_up.click()
+            time.sleep(0.5)
+            speed_down.click()
+            page.locator("text=restart ↻").wait_for()
+            context.close()
+            browser.close()
+
+        for video in temp_path.glob("*.webm"):
+            clip = mp.VideoFileClip(str(video.resolve()))
+            # Remove first portion of clip before terminhtml-js loads
+            clip = clip.subclip(1.1, clip.duration)
+            out_path = out_folder / f"demo-output.gif"
+            clip.write_gif(str(out_path.resolve()))
+            print(f"Demo output gif saved to {out_path}")
+
+
+if __name__ == "__main__":
+    create_demo_output_gif()
---
+++ docsrc/create-readme-gif.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ..
+python demo_video.py
+cd docsrc
